f1df9aa983fc87e332c05037c4534e8e82d6ca95
nuaacyy <32994526+nuaacyy@users.noreply.github.com>
2018-12-06 22:09:38 +0800

    first commit

---
+++ src/test/java/exercises/PersonInfo.java
@@ -0,0 +1,50 @@
+package exercises;
+
+public class PersonInfo {
+	private String name;
+	private String sex;
+	private int age;
+	private boolean isStudent;
+	private String[] hobbies;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public boolean isStudent() {
+		return isStudent;
+	}
+
+	public void setStudent(boolean isStudent) {
+		this.isStudent = isStudent;
+	}
+
+	public String[] getHobbies() {
+		return hobbies;
+	}
+
+	public void setHobbies(String[] hobbies) {
+		this.hobbies = hobbies;
+	}
+
+}

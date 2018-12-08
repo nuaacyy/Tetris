f8cb608bd077fdd840cc34a35e95b55df742892c
nuaacyy <32994526+nuaacyy@users.noreply.github.com>
2018-12-09 00:28:58 +0800

    添加AES高级加密算法注释

--- src/main/java/org/b3log/symphony/util/Crypts.java
+++ src/main/java/org/b3log/symphony/util/Crypts.java
@@ -36,71 +36,83 @@ import java.security.SecureRandom;
  */
 public final class Crypts {
 
-    /**
-     * Logger.
-     */
-    private static final Logger LOGGER = Logger.getLogger(Crypts.class);
+	/**
+	 * Logger.
+	 */
+	private static final Logger LOGGER = Logger.getLogger(Crypts.class);
+	/**
+	 * 使用到的演算法
+	 */
+	public static final String ALGORITHM = "AES";
+	/**
+	 * 编码规则
+	 */
+	public static final String CHARSET = "UTF-8";
 
-    /**
-     * Encrypts by AES.
-     *
-     * @param content the specified content to encrypt
-     * @param key the specified key
-     * @return encrypted content
-     * @see #decryptByAES(java.lang.String, java.lang.String)
-     */
-    public static String encryptByAES(final String content, final String key) {
-        try {
-            final KeyGenerator kgen = KeyGenerator.getInstance("AES");
-            final SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
-            secureRandom.setSeed(key.getBytes());
-            kgen.init(128, secureRandom);
-            final SecretKey secretKey = kgen.generateKey();
-            final byte[] enCodeFormat = secretKey.getEncoded();
-            final SecretKeySpec keySpec = new SecretKeySpec(enCodeFormat, "AES");
-            final Cipher cipher = Cipher.getInstance("AES");
-            final byte[] byteContent = content.getBytes("UTF-8");
-            cipher.init(Cipher.ENCRYPT_MODE, keySpec);
-            final byte[] result = cipher.doFinal(byteContent);
+	/**
+	 * Encrypts by AES.
+	 *
+	 * @param content the specified content to encrypt
+	 * @param key     the specified key
+	 * @return encrypted content
+	 * @see #decryptByAES(java.lang.String, java.lang.String)
+	 */
+	public static String encryptByAES(final String content, final String key) {
+		try {
+			final KeyGenerator kgen = KeyGenerator.getInstance(ALGORITHM);
+			final SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+			secureRandom.setSeed(key.getBytes());
+			kgen.init(128, secureRandom);
+			final SecretKey secretKey = kgen.generateKey();
+			final byte[] enCodeFormat = secretKey.getEncoded();
+			final SecretKeySpec keySpec = new SecretKeySpec(enCodeFormat, ALGORITHM);
+			final Cipher cipher = Cipher.getInstance(ALGORITHM);
+			final byte[] byteContent = content.getBytes(CHARSET);
+			cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+			final byte[] result = cipher.doFinal(byteContent);
 
-            return Hex.encodeHexString(result);
-        } catch (final Exception e) {
-            LOGGER.log(Level.WARN, "Encrypt failed", e);
+			return Hex.encodeHexString(result);
+		} catch (final Exception e) {
+			LOGGER.log(Level.WARN, "Encrypt failed", e);
 
-            return null;
-        }
-    }
+			return null;
+		}
+	}
 
-    /**
-     * Decrypts by AES.
-     *
-     * @param content the specified content to decrypt
-     * @param key the specified key
-     * @return original content
-     * @see #encryptByAES(java.lang.String, java.lang.String)
-     */
-    public static String decryptByAES(final String content, final String key) {
-        try {
-            final byte[] data = Hex.decodeHex(content.toCharArray());
-            final KeyGenerator kgen = KeyGenerator.getInstance("AES");
-            final SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
-            secureRandom.setSeed(key.getBytes());
-            kgen.init(128, secureRandom);
-            final SecretKey secretKey = kgen.generateKey();
-            final byte[] enCodeFormat = secretKey.getEncoded();
-            final SecretKeySpec keySpec = new SecretKeySpec(enCodeFormat, "AES");
-            final Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.DECRYPT_MODE, keySpec);
-            final byte[] result = cipher.doFinal(data);
+	/**
+	 * Decrypts by AES. aes解密 aes 即advanced encryption standard 高级加密标准
+	 * <p>
+	 * 是美国联邦政府采用的一种区块加密标准。这个标准用来替代原先的DES。2006年，高级加密标准已然成为<b>对称密钥加密</b>中最流行的算法之一。
+	 * </p>
+	 * 
+	 * @param content the specified content to decrypt
+	 * @param key     the specified key
+	 * @return original content
+	 * @see #encryptByAES(java.lang.String, java.lang.String)
+	 */
+	public static String decryptByAES(final String content, final String key) {
+		try {
+			final byte[] data = Hex.decodeHex(content.toCharArray());
+			// 实例化支持AES算法的密钥生成器（算法名称命名需按规定，否则抛出异常）
+			final KeyGenerator kgen = KeyGenerator.getInstance(ALGORITHM);
+			final SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+			secureRandom.setSeed(key.getBytes());
+			kgen.init(128, secureRandom);
+			final SecretKey secretKey = kgen.generateKey();
+			final byte[] enCodeFormat = secretKey.getEncoded();
+			final SecretKeySpec keySpec = new SecretKeySpec(enCodeFormat, ALGORITHM);
+			final Cipher cipher = Cipher.getInstance(ALGORITHM);
+			cipher.init(Cipher.DECRYPT_MODE, keySpec);
+			final byte[] result = cipher.doFinal(data);
 
-            return new String(result, "UTF-8");
-        } catch (final Exception e) {
-            LOGGER.log(Level.WARN, "Decrypt failed");
+			return new String(result, CHARSET);
+		} catch (final Exception e) {
+			LOGGER.log(Level.WARN, "Decrypt failed");
 
-            return null;
-        }
-    }
+			return null;
+		}
+	}
 
-    private Crypts() {
-    }
+	private Crypts() {
+	}
 }
